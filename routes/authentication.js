dd6f658427efca06ed6335d5cf67cfad2cd9cd6d
YuTakaki <takaki.yu.dev@gmail.com>
2021-02-13 21:53:08 +0900

    add verifyInfo middleware

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const authentication = require('./controllers/authetication_controller');
+const verifyInfo = require('../middleware/verifyInfo');
 
 const route = express.Router();
 
-route.post('/register', authentication.register);
+route.post('/register', verifyInfo, authentication.register);
 
 module.exports = route;
--- routes/controllers/authetication_controller.js
+++ routes/controllers/authetication_controller.js
@@ -3,18 +3,24 @@ const db = require('../../db');
 const register = async(req, res) => {
     console.log(req.body)
     try {
-        const {email, username, password} = req.body;
-        const newAccount = await db.query("INSERT INTO account (email, username, password) VALUES ($1, $2, $3) RETURNING * ",
-        [email, username, password]);
-        res.send(newAccount.rows);     
+        const {email, first_name, last_name, password, retry_password} = req.body;
+        if(password.length >= 8){
+            if(password === retry_password){
+                const newAccount = await db.query("INSERT INTO account (email, first_name, last_name, password) VALUES ($1, $2, $3, $4) RETURNING * ",
+                [email, first_name, last_name, password]);
+                res.send(newAccount.rows); 
+            }else{
+                res.send('password and retry password not the same');
+            }
+        }else{
+            res.send('password too short');
+        } 
     } catch (e) {
         console.log(e);
-        if(e.constraint === 'account_username_key'){
-            res.send('username already exist');
-        }else if(e.constraint === "password_length"){
-            res.send('password too short');
-        }else{
+        if(e.constraint === 'account_email_key"'){
             res.send('email already exist');
+        }else{
+            res.send('complete the form');
         }
     }
 }
