b45e971640b56dd464a3960ca6f2c83629dec2a4
YuTakaki <takaki.yu.dev@gmail.com>
2021-02-26 18:53:07 +0900

    finish v1.0

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
-    host : "localhost",
-    user : "postgres",
-    password : "yutakaki",
-    port : 5432,
-    database : "blog"
-});
+
+require('dotenv').config();
+
+const dev_config = {
+    host : process.env.PG_HOST,
+    user : process.env.PG_USER,
+    password : process.env.PG_PASSWORD,
+    port : process.env.PG_PORT,
+    database : process.env.PG_DATABASE
+}
+
+const pro_config = {
+    connectionString : process.env.DATABASE_URL
+}
+const pool = new Pool(process.env.NODE_ENV === 'production' ? pro_config : dev_config);
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
