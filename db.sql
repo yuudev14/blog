e9efbb56b88c83878aa83698591b2dede08d8451
YuTakaki <takaki.yu.dev@gmail.com>
2021-02-26 19:29:41 +0900

    added engines in packange.json

--- db.sql
+++ db.sql
@@ -6,7 +6,6 @@ CREATE table account (
     first_name  VARCHAR(50) NOT NULL,
     last_name  VARCHAR(50) NOT NULL,
     password VARCHAR(100) NOT NULL,
-    UNIQUE(username),
     UNIQUE(email)
 );
 
@@ -14,7 +13,7 @@ CREATE TABLE blogs (
     blog_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY ,
     date DATE NOT NULL DEFAULT now(),
     user_id uuid  NOT NULL REFERENCES account(user_id),
-    title VARCHAR(50) NOT NULL,
+    title TEXT NOT NULL,
     preview_img VARCHAR(100),
     blog TEXT[] NOT NULL
 );
@@ -30,7 +29,3 @@ CREATE TABLE reactions (
 CREATE TABLE activeTokens (
     token VARCHAR(500) NOT NULL PRIMARY KEY
 );
-
-SELECT react.count, blogs.title, blogs.preview_img FROM 
-(SELECT reactions.blog_id, COUNT(*) AS count FROM reactions JOIN blogs ON reactions.blog_id = blogs.blog_id GROUP BY reactions.blog_id) AS react
-JOIN blogs ON react.blog_id = blogs.blog_id ORDER BY react.count DESC;
