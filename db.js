52c479407a8bfca5e419f99c8a722f0c1c3fb90a
YuTakaki <takaki.yu.dev@gmail.com>
2021-02-27 20:16:07 +0900

    rearrage db.js

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
-const Pool = require('pg').Pool;
+const Pool = require('pg').Client;
 
 require('dotenv').config();
 
+// const dev_config = {
+//     host : process.env.PG_HOST,
+//     user : process.env.PG_USER,
+//     password : process.env.PG_PASSWORD,
+//     port : process.env.PG_PORT,
+//     database : process.env.PG_DATABASE
+// }
+
 const dev_config = {
-    host : process.env.PG_HOST,
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD,
-    port : process.env.PG_PORT,
-    database : process.env.PG_DATABASE
+    connectionString :`postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 }
 
 const pro_config = {
-    connectionString : process.env.DATABASE_URL
+    connectionString :process.env.PG_DATABASE,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 const pool = new Pool(process.env.NODE_ENV === 'production' ? pro_config : dev_config);
 
